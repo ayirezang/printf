2ac6bbba7cee1d11b11d397e018ad1585a6e05a1
ayirezang <patienceayirezang33@gmail.com>
2023-12-06 11:36:54 -0800

    printf_.c

--- printf_.c
+++ printf_.c
@@ -1,38 +1,48 @@
 #include "main.h"
 
+/**
+ * printf - it is suppose to to take const char string
+ * format - this is a format string an argument for printf
+ * this function is suppose to retun an integer
+ */
 
 
-int _printf(const char *format,...)
+
+int _printf(const char *format,	...)
 {
 
 	int charchar = 0;
 
 	va_list myownlist;
+
 	va_start(myownlist, format);
-	/* check if the format specifier encountered is 'c'
-	 * then write to the standard output stream.
-	 */
 
-	if(format[0] == c)
-	{	
-		write(1,charchar,1);
-	}
-	va_arg(myownlist,char);
+	/**
+	*check if the format specifier encountered is 'c'
+	*then write to the standard output stream.
+	*/
 
-	else if(format[0] == s);
+	if (format[0] == c)
 	{
-		write(1,format,strlen(format));
+		write(1, charchar, 1);
+	}
+	va_arg(myownlist, char);
+
+
+	else if (format[0] == s);
+	{
+		write(1, format, strlen(format));
 	}
 
-	va_arg(myownlist,char);
-	else if(format[0] == %)
+	va_arg(myownlist, char);
+	else if (format[0] == %)
 	{
-		write(1,format,1);
+		write(1, format, 1);
 	}
-	else 
+	else
 	{
 		return (-1);
-	}	
+	}
 	va_end(myownlist);
-	return charchar;
+	return (charchar);
 }
