63321c437945fce0992eac742e27abd254bb6628
ayirezang <patienceayirezang33@gmail.com>
2023-12-06 12:19:30 -0800

    printf_.c

--- printf_.c
+++ printf_.c
@@ -14,7 +14,7 @@ int _printf(const char *format,	...)
 
 	int charchar = 0;
 
-	va_list myownlist;
+	va_list(myownlist);
 
 	va_start(myownlist, format);
 
@@ -23,20 +23,20 @@ int _printf(const char *format,	...)
 	*then write to the standard output stream.
 	*/
 
-	if (format[0] == c)
+	if (format[0] == 'c')
 	{
 		write(1, charchar, 1);
 	}
 	va_arg(myownlist, char);
 
 
-	else if (format[0] == s);
+	else if (format[0] == 's');
 	{
 		write(1, format, strlen(format));
 	}
 
 	va_arg(myownlist, char);
-	else if (format[0] == %)
+	else if (format[0] == '%')
 	{
 		write(1, format, 1);
 	}
